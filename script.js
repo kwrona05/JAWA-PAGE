6618e17b8cc71c86dca953b56a6675632641b0a7
kwrona05 <kwrona05@gmail.com>
2024-04-29 09:01:26 +0200

    add button .readMore to script.js

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 document.getElementById('gallery').addEventListener('click', goGallery)
 function goGallery() {
     window.location.href = "gallery.html"
-}
+};
+document.getElementById('readMore').addEventListener('click', goRead)
+function goRead() {
+    window.location.href = "readMore.html"
+};
